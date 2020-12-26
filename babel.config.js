92fa2c9af65ffbd75264eddf572c463c975a3cb2
Jumhsmhs <59906009+Jumhsmhs@users.noreply.github.com>
2020-12-26 15:00:30 +0700

    เริ่มทดลอง

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"]
 };
+
+module.exports = {
+  resolve: {
+    alias: {
+      // Alias for using source of BootstrapVue
+      'bootstrap-vue$': 'bootstrap-vue/src/index.js'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // Exclude transpiling `node_modules`, except `bootstrap-vue/src`
+        exclude: /node_modules\/(?!bootstrap-vue\/src\/)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+}
---
+++ src/views/Createaccount.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div class="createaccount">
+    <h1>SIGN IN</h1>
+      <div class="mb-3">
+       <label for="exampleFormControlInput1" class="form-label">Username</label>
+         <input type="username" class="form-control" id="exampleFormControlInput1" placeholder="username">
+      </div>
+      <div class="mb-3">
+       <label for="exampleFormControlInput1" class="form-label">E-mail</label>
+         <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@mail.com">
+      </div>
+      <div class="mb-3">
+       <label for="exampleFormControlInput1" class="form-label">Password</label>
+         <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Password">
+      </div>
+      <div class="mb-3">
+       <label for="exampleFormControlInput1" class="form-label">Repeat Password</label>
+         <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Repeat Password">
+      </div>
+    <div>
+        <button type="button" class="btn btn-outline-info">Create Account</button>
+    </div>
+  </div>
+</template>
