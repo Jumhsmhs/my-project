e9ed9f4bc7e2b080b0b19b8cae976b1d20912957
Jumhsmhs <59906009+Jumhsmhs@users.noreply.github.com>
2020-12-26 16:30:45 +0700

    แก้มานิดเดียว

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,16 +2,22 @@
 
   <div class="Login">
     <h1>SIGN IN</h1>
-            <div class="mb-3">
+     <br>
+      <br>
+            <div class="d-grid gap-2 col-3 mx-auto">
                 <label for="exampleFormControlInput1" class="form-label">Username</label>
                     <input type="username" class="form-control" id="exampleFormControlInput1" placeholder="username">
             </div>
-            <div class="mb-3">
+             <br>
+            <div class="d-grid gap-2 col-3 mx-auto">
                 <label for="exampleFormControlInput1" class="form-label">Password</label>
                     <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Password">
             </div>
+        <br>
         <div>
            <router-link to="/Homenewpage"><button type="button" class="btn btn-outline-info">Login</button></router-link>
         </div>
+
   </div>
 </template>
+
